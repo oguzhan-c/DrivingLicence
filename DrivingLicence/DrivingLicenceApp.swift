63389abb64e022038061174347b0a66723b7247f
Oğuzhan Can <oguzhan.can.iletisim@gmail.com>
2023-04-21 17:52:20 +0300

    Initial Commit

---
+++ DrivingLicence/DrivingLicenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrivingLicenceApp.swift
+//  DrivingLicence
+//
+//  Created by Oğuzhan Can on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DrivingLicenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
